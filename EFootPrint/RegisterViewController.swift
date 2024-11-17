49a837d3c79e718369f8bf503b6dc0c26852d748
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2024-11-17 14:58:04 +0000

    Updated View controller

---
+++ EFootPrint/RegisterViewController.swift
@@ -0,0 +1,16 @@
+//
+//  RegisterViewController.swift
+//  EFootPrint
+//
+//  Created by Joseph Mccole on 16/11/2024.
+//
+
+import UIKit
+
+class RegisterViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
