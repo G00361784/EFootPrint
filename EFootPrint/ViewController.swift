4f45552cf31f893074f5213bd0720f86204a95b7
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2024-11-21 16:29:56 +0000

    Added feature register users

--- EFootPrint/ViewController.swift
+++ EFootPrint/ViewController.swift
@@ -6,22 +6,51 @@
 //
 
 import UIKit
-
+import FirebaseCore
+import FirebaseAuth
 class ViewController: UIViewController {
+    func showError(_ message: String) {
+        let alert = UIAlertController(title: "Error", message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: .default))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    
+    
     @IBOutlet weak var LoginOutlet: UITextField!
     
     @IBOutlet weak var passwordOutlet: UITextField!
     
-    
-    @IBAction func LoginText(_ sender: UITextField) {
+    @IBAction func LoginPressed(_ sender: UIButton) {
+        if let email = LoginOutlet.text, let password = passwordOutlet.text {
+            
+            // Ensure email and password are not empty
+            if email.isEmpty || password.isEmpty {
+                // Display an error message to the user (e.g., using an alert)
+                showError("Please enter both email and password.")
+                return
+            }
+            
+            if password.count < 6 {
+                // Display an error for weak password
+                showError("Password must be at least 6 characters long.")
+                return
+            }
+            
+            // Create user with Firebase Auth
+            Auth.auth().createUser(withEmail: email, password: password) { authResult, error in
+                if let e = error {
+                    // Display error to the user
+                    self.showError(e.localizedDescription)
+                } else {
+                    // Success! Perform the segue
+                   self.performSegue(withIdentifier: "toHomeScreen", sender: self)
+                }
+            }
+        } else {
+            // Handle the case where email or password are nil (shouldn't happen)
+            showError("Email or password cannot be nil.")
+        }
     }
-    @IBAction func PasswordText(_ sender: UITextField) {
-    }
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
-    }
-
-
+        
 }
-
