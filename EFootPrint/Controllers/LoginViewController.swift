e545df575631db8e083af6b8a19919ac8c135d20
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2024-11-27 15:26:03 +0000

    updates to main.storyboard and login script

--- EFootPrint/Controllers/LoginViewController.swift
+++ EFootPrint/Controllers/LoginViewController.swift
@@ -20,7 +20,7 @@ class LoginViewController: UIViewController {
             Auth.auth().signIn(withEmail: email, password: password) { [weak self] authResult, error in
                 guard let strongSelf = self else { return }
                 if let e = error{
-                    print(e)
+                    print(e.localizedDescription)
                 }else{
                     self?.performSegue(withIdentifier: "toHomeScreenFromL", sender: self)
                 }
