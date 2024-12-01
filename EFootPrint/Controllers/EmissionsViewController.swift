b9daa8d92191e189c1bfc11c572c8c7e5e487997
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2024-12-01 18:49:32 +0000

    started to build step count functionality

--- EFootPrint/Controllers/EmissionsViewController.swift
+++ EFootPrint/Controllers/EmissionsViewController.swift
@@ -6,24 +6,76 @@
 //
 
 import UIKit
+import HealthKit
 
 class EmissionsViewController: UIViewController {
 
+    @IBOutlet weak var stepCountLabel: UILabel!
+    let healthStore = HKHealthStore()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        authorizeHealthKit()
+        showPermissionAlert()
     }
-    
+    func showPermissionAlert() {
+            let alertController = UIAlertController(title: "Allow Access to HealthKit?",
+                                                    message: "This app needs access to your step data to track your activity.",
+                                                    preferredStyle: .alert)
 
-    /*
-    // MARK: - Navigation
+            let allowAction = UIAlertAction(title: "Allow", style: .default) { _ in
+                self.requestHealthKitAuthorization()
+            }
 
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+            alertController.addAction(allowAction)
+            present(alertController, animated: true, completion: nil)
+        }
+
+        func requestHealthKitAuthorization() {
+            let healthDataTypes = Set([HKQuantityType.quantityType(forIdentifier: .stepCount)!])
+
+            healthStore.requestAuthorization(toShare: [], read: healthDataTypes) { (success, error) in
+                if success {
+                    self.getStepCount()
+                } else {
+                    // Handle authorization error (e.g., show an error message)
+                    if let error = error {
+                        print("HealthKit authorization failed with error: \(error)")
+                        // You could display an alert here to inform the user.
+                    }
+                }
+            }
+        }
+
+    func authorizeHealthKit() {
+        let healthDataTypes = Set([HKQuantityType.quantityType(forIdentifier: .stepCount)!])
+
+        healthStore.requestAuthorization(toShare: [], read: healthDataTypes) { (success, error) in
+            if success {
+                self.getStepCount()
+            }
+        }
     }
-    */
 
+    func getStepCount() {
+        let stepCountType = HKQuantityType.quantityType(forIdentifier: .stepCount)!
+
+        let query = HKStatisticsQuery(quantityType: stepCountType,
+                                    quantitySamplePredicate: nil,
+                                    options: .cumulativeSum) { _, result, error in
+            guard let result = result else {
+                // Handle error
+                return
+            }
+
+            if let stepCount = result.sumQuantity() {
+                let stepCountDouble = stepCount.doubleValue(for: HKUnit.count())
+                DispatchQueue.main.async {
+                    self.stepCountLabel.text = "\(Int(stepCountDouble)) steps"
+                }
+            }
+        }
+
+        healthStore.execute(query)
+    }
 }
