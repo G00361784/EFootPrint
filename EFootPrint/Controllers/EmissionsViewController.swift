f7f715d522f1fc53446b92837502f0d69e5022eb
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2024-12-02 20:00:52 +0000

    commit to add transport type functionality

--- EFootPrint/Controllers/EmissionsViewController.swift
+++ EFootPrint/Controllers/EmissionsViewController.swift
@@ -11,8 +11,32 @@ import HealthKit
 class EmissionsViewController: UIViewController {
 
     @IBOutlet weak var stepCountLabel: UILabel!
+    @IBOutlet weak var walkVsCar: UILabel!
     let healthStore = HKHealthStore()
 
+    func calculateCarbonSavings(distanceInMiles: Double, transportationType: String) -> Double {
+
+        let emissionsPerMile: Double
+
+        switch transportationType {
+        case "car":
+            emissionsPerMile = 404  // grams of CO2 per mile for a typical gasoline car
+        case "bus":
+            emissionsPerMile = 105  // grams of CO2 per mile for a typical bus (average)
+        case "train":
+            emissionsPerMile = 35   // grams of CO2 per mile for a typical train (average)
+        case "walk":
+            emissionsPerMile = 0
+        default:
+            emissionsPerMile = 0    // Assume no emissions for walking or other modes
+        }
+
+        let carbonSavings = distanceInMiles * emissionsPerMile
+
+        return carbonSavings
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         authorizeHealthKit()
@@ -45,12 +69,20 @@ class EmissionsViewController: UIViewController {
                 let stepCount = sum.doubleValue(for: HKUnit.count())
                 print("Total steps: \(stepCount)")
                 // Update UI on main thread
+                let calculationPerformed = self.calculateCarbonSavings(distanceInMiles:stepCount/2250.00, transportationType: "car");//average step count per mile is 2250
+                
+                
                 DispatchQueue.main.async {
                     self.stepCountLabel.text = "Total steps: \(stepCount)"
+                    self.walkVsCar.text = "\(calculationPerformed)"
+                    
                 }
+                
             }
         }
 
         healthStore.execute(query)
+        
+       
     }
 }
