04ba2bd936912700770b4363b1b30241f9fb56b1
Joseph McCole <123952544+G00361784@users.noreply.github.com>
2024-12-05 14:50:06 +0000

    updates to UI

---
+++ EFootPrint/Controllers/WelcomeScreenViewController.swift
@@ -0,0 +1,19 @@
+//
+//  WelcomeScreenViewController.swift
+//  EFootPrint
+//
+//  Created by Joseph Mccole on 04/12/2024.
+//
+
+import UIKit
+
+class WelcomeScreenViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    // Helper function to create content views
+    
+    }
